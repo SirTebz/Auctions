b8c56fb679ef6a829584e1a20331077244b15b6a
SirTebz <mokgositeboho77@gmail.com>
2024-10-18 15:06:12 +0200

    Initial Commit

--- Auctions/Data/ApplicationDbContext.cs
+++ Auctions/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Auctions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Auctions.Data
@@ -8,6 +9,10 @@ namespace Auctions.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        public DbSet<Listing> Listings { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
